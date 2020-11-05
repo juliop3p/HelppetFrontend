1dba2457b9fecbd57885f5a1c6bd46a511df4a0d
juliop3p <julio15.zn@gmail.com>
2020-11-05 11:01:03 -0300

    insert font awesome

--- frontend/src/app/components/footer/footer.component.ts
+++ frontend/src/app/components/footer/footer.component.ts
@@ -1,22 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-import { faFacebook } from '@fortawesome/free-brands-svg-icons'
-import { faInstagram } from '@fortawesome/free-brands-svg-icons'
-import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faFacebook } from '@fortawesome/free-brands-svg-icons';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
 })
 export class FooterComponent implements OnInit {
-  
-  faFacebook = faFacebook
-  faInstagram = faInstagram
-  faLinkedin = faLinkedin
-  
-  constructor() { }
+  faFacebook = faFacebook;
+  faInstagram = faInstagram;
+  faLinkedin = faLinkedin;
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
