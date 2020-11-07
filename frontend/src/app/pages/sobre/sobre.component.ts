ad900c57fced522a0b92144491e85c07a40cb84d
Deborah Evelyn Carlota <evelyndec92@gmail.com>
2020-11-07 16:13:42 -0300

    Upate nas redes sociais da página sobre

--- frontend/src/app/pages/sobre/sobre.component.ts
+++ frontend/src/app/pages/sobre/sobre.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { faFortAwesome } from '@fortawesome/free-brands-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-sobre',
@@ -6,6 +9,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sobre.component.css']
 })
 export class SobreComponent implements OnInit {
+
+  faFortAwesome = faFortAwesome
+  faGithub = faGithub
+  faLinkedin = faLinkedin
 
   constructor() { }
 
